cdfccc057430192b37332a45f4d76effed35e8d1
Frank J. Gómez <frank@ginkgostreet.com>
2017-06-14 15:59:27 -0400

    VARL-189: Work in progress. First pass at API call (and default parameters) for creating mailing.

--- api/v3/MatchMail/Create.php
+++ api/v3/MatchMail/Create.php
@@ -4,12 +4,51 @@
  * MatchMail.Create API specification (optional)
  * This is used for documentation and validation.
  *
- * @param array $spec description of fields supported by this API call
+ * @param array $spec
+ *   Description of fields supported by this API call.
  * @return void
  * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
  */
 function _civicrm_api3_match_mail_Create_spec(&$spec) {
-  $spec['magicword']['api.required'] = 1;
+  $spec['created_id'] = array(
+    'api.default' => 6964, // the contact ID of the cron user
+    'description' => 'FK to Contact ID who first created this mailing',
+    'title' => 'Mailing Creator',
+    'type' => CRM_Utils_Type::T_INT,
+    'FKApiName' => 'Contact',
+  );
+  $spec['name'] = array(
+    'api.default' => 'Weekly Volunteer Match ' . date('Y-m-d'),
+    'description' => 'Mailing Name (used in backend interfaces only)',
+    'title' => 'Mailing Name',
+  );
+  $spec['from_name'] = array(
+    'api.default' => 'Volunteer Arlington',
+    'description' => 'From Header of mailing',
+    'title' => 'Mailing From Name',
+  );
+  $spec['from_email'] = array(
+    'api.default' => 'volarl@volunteerarlington.org',
+    'description' => 'From Email of mailing',
+    'title' => 'Mailing From Email',
+  );
+  $spec['subject'] = array(
+    'api.default' => 'Volunteer opportunities in and around Arlington this week',
+    'description' => 'Subject of mailing',
+    'title' => 'Subject',
+  );
+  $spec['msg_template_id'] = array(
+    'api.default' => /* TODO API lookup */70,
+    'description' => 'FK to the message template',
+    'title' => 'Mailing Message Template',
+  );
+  $spec['target_group'] = array(
+    'api.required' => TRUE,
+    'description' => 'FK to Group ID to which the mailing should be sent',
+    'title' => 'Target Group',
+    'type' => CRM_Utils_Type::T_INT,
+    'FKApiName' => 'Group',
+  );
 }
 
 /**
@@ -22,20 +61,25 @@ function _civicrm_api3_match_mail_Create_spec(&$spec) {
  * @throws API_Exception
  */
 function civicrm_api3_match_mail_Create($params) {
-  if (array_key_exists('magicword', $params) && $params['magicword'] == 'sesame') {
-    $returnValues = array(
-      // OK, return several data rows
-      12 => array('id' => 12, 'name' => 'Twelve'),
-      34 => array('id' => 34, 'name' => 'Thirty four'),
-      56 => array('id' => 56, 'name' => 'Fifty six'),
-    );
-    // ALTERNATIVE: $returnValues = array(); // OK, success
-    // ALTERNATIVE: $returnValues = array("Some value"); // OK, return a single value
-
-    // Spec: civicrm_api3_create_success($values = 1, $params = array(), $entity = NULL, $action = NULL)
-    return civicrm_api3_create_success($returnValues, $params, 'NewEntity', 'NewAction');
-  }
-  else {
-    throw new API_Exception(/*errorMessage*/ 'Everyone knows that the magicword is "sesame"', /*errorCode*/ 1234);
-  }
+  civicrm_api3('Mailing', 'create', array(
+    'created_id' => $params['created_id'],
+    'name' => $params['name'],
+    'mailing_type' => 'standalone',
+    'from_name' => $params['from_name'],
+    'from_email' => $params['from_email'],
+    'subject' => $params['subject'],
+//    'body_html' => "Maybe unnecessary if the template does the lifting?",
+    'url_tracking' => 1,
+    'auto_responder' => 0,
+    'open_tracking' => 1,
+    'msg_template_id' => $params['msg_template_id'],
+    'visibility' => 'User and User Admin Only',
+    'dedupe_email' => 1,
+    'email_selection_method' => 'automatic',
+    'api.MailingGroup.create' => array(
+      'entity_id' => $params['target_group'],
+      'entity_table' => 'civicrm_group',
+      'group_type' => 'Include',
+    ),
+  ));
 }
